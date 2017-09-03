5348cb51f981a18ba67e6fb425d4fb7768083fec
Fred Erik <stammix@web.de>
2017-09-03 14:04:10 +0200

    first commit of the new project

---
+++ BBallStatsFromScratch/SettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BBallStatsFromScratch
+//
+//  Created by Fred Erik on 03.09.17.
+//  Copyright © 2017 Fred Erik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
